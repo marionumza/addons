f13165bc5dda7ecf56cd2e4bffe0b6b3dbe2e503
Mario Osvaldo Nuñez <marionumza@gmail.com>
2020-11-10 19:44:13 -0300

    Subida de modulos para formio

---
+++ formio_data_api/__manifest__.py
@@ -0,0 +1,27 @@
+# Copyright Nova Code (http://www.novacode.nl)
+# See LICENSE file for full licensing details.
+
+{
+    'name': 'Form.io Data API',
+    'summary': 'Python API for Form.io data (builder, form/submission).',
+    'version': '0.5',
+    'license': 'LGPL-3',
+    'author': 'Nova Code',
+    'website': 'https://www.novacode.nl',
+    'live_test_url': 'https://demo13.novacode.nl',
+    'category': 'Extra Tools',
+    'depends': ['formio'],
+    'data': [],
+    'external_dependencies': {
+        'python': ['formiodata'],
+    },
+    'application': False,
+    'images': [
+        'static/description/banner.png',
+    ],
+    'description': """
+Form.io Data API
+================
+
+"""
+}
---
+++ formio_report_qweb/__manifest__.py
@@ -0,0 +1,25 @@
+# Copyright Nova Code (http://www.novacode.nl)
+# See LICENSE file for full licensing details.
+
+{
+    'name': 'Form.io QWeb Reports',
+    'summary': 'Generate (PDF) reports for every Form',
+    'version': '0.9',
+    'author': 'Nova Code',
+    'website': 'https://www.novacode.nl',
+    'license': 'LGPL-3',
+    'category': 'Extra Tools',
+    'depends': ['formio', 'formio_data_api'],
+    'data': [
+        'report/formio_form_report_views.xml',
+        'report/report_formio_form.xml',
+        'report/report_formio_form_components.xml',
+        'security/ir_model_access.xml',
+        'views/formio_builder_views.xml',
+        'views/formio_builder_report_views.xml'
+    ],
+    'application': True,
+    'images': [
+        'static/description/banner.png',
+    ],
+}
